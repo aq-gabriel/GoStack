c99eccf4a917bdafedfa173379958b82889387f3
Gabriel Aquino <gabrielaquino.tec@gmail.com.br>
2020-11-08 20:27:23 -0300

    docs: separating folder

---
+++ segunda-etapa/primeiro-projeto-node/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/index'
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+
+app.listen(port, () => {
+  console.log(`🚀 Listen http://localhost:3333/  🚀`);
+});
